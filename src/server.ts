37b65b878f35e12d72a59c91680fa00e2a665527
Josue Pimentel <josue.farias.pimentel@gmail.com>
2025-06-28 18:37:54 -0300

    feat: arrumando para build

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import 'dotenv/config';
 import cors from '@fastify/cors';
 
 app.register(cors, {
-  origin: 'http://127.0.0.1:5500',
+  origin: '*',
   methods: ['PATCH', 'POST', 'GET'],
 });
 
 app.listen({
+  host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   port: Number(process.env.PORT) ?? 3000
 }, () => console.log(`HTTP Server Running in port ${process.env.PORT ?? 3000}`));
